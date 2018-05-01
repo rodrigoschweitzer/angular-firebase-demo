b4d3c54281cf5e54d8b318088aac9394fbf13d1c
Rodrigo Schweitzer <rodrigopsschweitzer@gmail.com>
2018-05-01 23:01:22 +0100

    Adding TodoHeaderComponent and TodoListComponent to TodoModule

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { TodoRoutingModule, ROUTE_COMPONENTS } from './todo-routing.module';
 import { SharedModule } from '@afd-shared/shared.module';
+import { NgModule } from '@angular/core';
+
+import { TodoHeaderComponent } from './header/todo-header.component';
+import { TodoListComponent } from './list/todo-list.component';
+import { ROUTE_COMPONENTS, TodoRoutingModule } from './todo-routing.module';
 
 @NgModule({
   imports: [
     SharedModule,
     TodoRoutingModule
   ],
-  declarations: [ROUTE_COMPONENTS]
+  declarations: [
+    ROUTE_COMPONENTS,
+    TodoHeaderComponent,
+    TodoListComponent
+  ]
 })
 export class TodoModule { }
